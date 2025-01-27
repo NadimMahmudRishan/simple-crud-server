8141cf97db8e4e4a6a0d6437a3498864512696a4
NadimMahmudRishan <resunroy420@gmail.com>
2025-01-28 01:33:38 +0600

    Data can uploaded into database.

--- index.js
+++ index.js
@@ -9,6 +9,50 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Start
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://rishan35952:kFEens8Rjmmdwv8h@cluster0.bpllr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db("usersDB");
+    const userCollection = database.collection("users");
+
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("New user : ", user);
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+// end
+
 app.get("/", (req, res) => {
   res.send("SIMPLE CRUD IS RUNNING");
 });
